86d6ee36cf8af9fca5eef58b1adcb33e5a60649b
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-27 21:56:44 -0500

    Add middleware to require user authentication

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import { requireAuth } from "../middleware/requireAuth";
 import { getProfiles, createProfile } from "../controllers/profileController";
 
 const router = express.Router();
+
+// Require authentication for all Profile routes
+router.use(requireAuth);
 
 // GET all User Profiles
 router.get("/", getProfiles);
