4a6f374a6b7ce47d4538c958ddd5f136c3259329
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-02-21 00:08:19 -0500

    Modify getAllLikes to return user_id

--- src/controllers/likesController.ts
+++ src/controllers/likesController.ts
@@ -2,7 +2,7 @@ import { pool } from "../elephantsql";
 
 export const getAllLikes = async (req, res) => {
   const likes = await pool.query(
-    `SELECT l.id, l.post_id, pr.first_name, pr.last_name, pr.profile_picture 
+    `SELECT l.id, l.post_id, l.user_id, pr.first_name, pr.last_name, pr.profile_picture 
     FROM Likes l 
     INNER JOIN Posts po on l.post_id = po.id 
     INNER JOIN Profiles pr on l.user_id = pr.user_id`
