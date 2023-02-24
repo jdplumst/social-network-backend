272e6180f47c8b6c2eb83c169e656b4e6eaacc56
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-02-23 19:04:47 -0500

    Modify getComments to get Comments in order by modify_date

--- src/controllers/commentsController.ts
+++ src/controllers/commentsController.ts
@@ -5,7 +5,8 @@ export const getComments = async (req, res) => {
   const comments = await pool.query(
     `SELECT c.id, c.post_id, c.user_id, c.description, c.create_date, c.modify_date, pr.first_name, pr.last_name, pr.profile_picture 
     FROM Comments c 
-    INNER JOIN Profiles pr on c.user_id = pr.user_id`
+    INNER JOIN Profiles pr on c.user_id = pr.user_id
+    ORDER BY c.modify_date ASC`
   );
   res.status(200).json(comments.rows);
 };
